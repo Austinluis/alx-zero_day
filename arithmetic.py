ea28eea84e0b712a6895018f067c8433061671bc
Austinluis <ogunsanyalouis@gmail.com>
2023-10-13 11:25:10 +0100

    lets lend this space

---
+++ arithmetic.py
@@ -0,0 +1,12 @@
+# Script name: arithmetic.py
+# Purpose: a simple calculator that takes two numbers, performs some arithmetic opererations and outputs the result
+# The arithmetic operations include: addition, subtraction, multiplication, division and exponential
+# Author: Ogunsanya Louis 236345
+
+import math
+
+# collects input 
+first_number = int[input("Enter the first number:")]
+second_number = int[input("Enter the second number:")]
+
+# assigns the answer
